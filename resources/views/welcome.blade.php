e756e0c6c3bcb515907996ee071a93b84d4bc82a
reiswitchblade05 <reiboyot05@gmail.com>
2023-06-14 06:16:19 +0700

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,116 +1,115 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bimbingan Konseling</title>
-    <link rel="stylesheet" href="{{ asset('/css/landing-page.css') }}">
-    <link href="https://fonts.cdnfonts.com/css/graphik" rel="stylesheet">
+    <meta charset="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>Desktop - 2</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A800" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A500%2C600%2C700%2C800" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat%3A500%2C600%2C700%2C800" />
+    <link rel="stylesheet" href="./styles/desktop-2.css" />
 </head>
 
 <body>
-    <header>
-        <nav>
-            <div class="container">
-                <a href="#" class="logo"><img src="{{ asset('/images/logo.png') }}" alt=""></a>
-                <input type="checkbox" id="menu-toggle" />
-                <label for="menu-toggle" class="menu-icon">&#9776;</label>
-                <ul class="menu">
-                    <li><a href="#about">Tentang</a></li>
-                    <li><a href="#services">Layanan</a></li>
-                    <li><a href="#contact">Kontak</a></li>
-                    <li><a href="<?= url('login'); ?>" class="btn-login">Login</a></li>
-                </ul>
+    <div class="desktop-2-oeN">
+        <div class="auto-group-xvas-Eza">
+            <div class="rectangle-10-KuY">
             </div>
-        </nav>
-    </header>
-
-    <section class="hero" id="hero">
-        <div class="hero-container">
-            <h1>Selamat Datang di SAC</h1>
-            <p>Dapatkan bantuan profesional untuk masalah pribadi anda <br> di Starbhak Advocacy & Care, platform yang
-                didedikasikan <br> untuk Bimbingan Konseling.</p>
-            <a href="#contact" class="btn">Hubungi Kami</a>
-        </div>
-        <img src="{{ asset('/images/Consulting_Flatline.png') }}" alt="">
-    </section>
-
-    <section id="about" style="margin-top: 5vw;">
-        <div class="container">
-            <h2>Tentang Kami</h2>
-            <p>
-                Starbhak Advocacy & Care adalah sebuah layanan profesional yang menyediakan dukungan dan bimbingan <br>
-                konseling
-                untuk membantu Anda mengatasi masalah pribadi, seperti stres, kecemasan, depresi, dan lainnya.
+            <div class="image-3-qN6">
+            </div>
+            <div class="group-4-hQJ"><a href="{{route('login')}}" style="color: #fff; text-decoration: none;">Login</a></div>
+            <img class="sac-1-TXt" src="./assets/sac-1.png" />
+            <p class="complete-mental-healt-care-solution-for-everyone-z22">
+                Complete Mental Health
+                <br />
+                Care Solution For Everyone
             </p>
         </div>
-    </section>
-
-    <section class="visimisi" id="visimisi">
-        <img src="{{ asset('/images/Conversation _Flatline.png') }}" alt="">
-        <div class="visimisi-container">
-            <h2 style="margin-bottom: 10px;">Visi & Misi</h2>
-            <p>
-                <b>Visi</b> <br>
-                Visi SAC adalah terwujudnya kehidupan kemanusiaan <br> yang membahagiaan melalui tersedianya pelayanan
-                <br> bimbingan konseling dalam pemberian dukungan <br> perkembangan dan pengetasan masalah agar peserta
-                didik berkembang secara optimasi, mandiri dan bahagia.
-            </p>
-            <p>
-                <b>Misi</b> <br>
-                <b>1.</b> Misi pendidikan, yaitu memfasilitasi pengembangan peserta didik melalui pembentukan perilaku
-                efektif-nromatif dalam kehidupan keseharian dan masa depan. <br>
-                <b>2.</b> Misi pengembangan, yaitu memfasilitasi pengembangan potensi dan kompetensi peserta didik dalam
-                lingkungan sekolah/madrasah, keluarga dan masyarakat. <br>
-                <b>3.</b> Misi menuntaskan masalah, yaitu memfasilitasi menuntaskan masalah peserta didik mengacu pada
-                kehidupan efektif sehari-hari. <br>
-            </p>
-        </div>
-    </section>
-
-    <section id="services">
-        <div class="container">
-            <h2 style="margin-bottom: 1.5vw;">Layanan Kami</h2>
-            <div class="service">
-                <h3>Konseling Pribadi</h3>
-                <p>
-                    Kami menawarkan konseling pribadi yang disesuaikan dengan kebutuhan Anda. Anda akan bekerja dengan
-                    konselor terlatih yang akan membantu Anda menemukan solusi terbaik untuk masalah yang Anda hadapi.
-                </p>
+        <div class="auto-group-t2kx-SPp">
+            <div class="auto-group-5lpg-vZt">
+                <div class="auto-group-psx6-cSi">
+                    <p class="mari-peduli-kesehatan-mentalmu-mulai-dari-sekarang-ugi">
+                        Mari Peduli Kesehatan Mentalmu
+                        <br />
+                        Mulai Dari Sekarang
+                    </p>
+                    <p class="selamat-datang-di-layanan-konseling-bk-online-kami-adalah-platform-yang-didedikasikan-untuk-memberikan-layanan-konseling-bimbingan-dan-konseling-bk-secara-daring-kepada-siswa-orang-tua-dan-guru-kami-memahami-pentingnya-mendukung-kesejahteraan-emosional-dan-perkembangan-pribadi-dalam-konteks-pendidikan-dan-kami-bertujuan-untuk-memberikan-akses-mudah-dan-terjangkau-ke-layanan-konseling-yang-berkualitas-1jk">Selamat datang di Layanan Konseling BK Online! Kami adalah platform yang didedikasikan untuk memberikan layanan konseling bimbingan dan konseling (BK) secara daring kepada siswa, orang tua, dan guru. Kami memahami pentingnya mendukung kesejahteraan emosional dan perkembangan pribadi dalam konteks pendidikan, dan kami bertujuan untuk memberikan akses mudah dan terjangkau ke layanan konseling yang berkualitas.</p>
+                </div>
+                <img class="item-3892061-1-NTp" src="./assets/-hdQ.png" />
             </div>
-            <div class="service">
-                <h3>Konseling Kelompok</h3>
-                <p>
-                    Selain konseling pribadi, kami juga menyediakan konseling kelompok yang memungkinkan Anda berbagi
-                    pengalaman dan mendapatkan dukungan dari orang lain yang mengalami masalah serupa.
-                </p>
+            <div class="auto-group-gmjn-gDc">
+                <img class="item-6844076-1-Q9c" src="./assets/.png" />
+                <div class="auto-group-lgrl-8LW">
+                    <p class="bagaimana-memulai-konseling-dengan-guru-bk-Fg2">Bagaimana Memulai Konseling dengan Guru BK? </p>
+                    <p class="untuk-memulai-menggunakan-layanan-konseling-bk-online-kami-anda-dapat-mendaftar-di-situs-web-kami-dan-membuat-akun-setelah-itu-anda-dapat-menjadwalkan-sesi-konseling-dengan-konselor-yang-tersedia-sesuai-dengan-waktu-yang-nyaman-bagi-anda-kami-menawarkan-fleksibilitas-dalam-jadwal-agar-anda-dapat-memilih-waktu-yang-sesuai-dengan-kebutuhan-anda-9mQ">Untuk memulai menggunakan layanan konseling BK online kami, Anda dapat mendaftar di situs web kami dan membuat akun. Setelah itu, Anda dapat menjadwalkan sesi konseling dengan konselor yang tersedia, sesuai dengan waktu yang nyaman bagi Anda. Kami menawarkan fleksibilitas dalam jadwal agar Anda dapat memilih waktu yang sesuai dengan kebutuhan Anda.</p>
+                </div>
+            </div>
+            <p class="pelayanan-kami-7bk">Pelayanan Kami</p>
+            <div class="auto-group-zb2w-QKx">
+                <div class="group-6-vp6">
+                    <p class="bimbingan-pribadi-F5g">Bimbingan Pribadi</p>
+                    <img class="item-8750075-1-ko8" src="./assets/-XK4.png" />
+                </div>
+                <div class="group-7-pnz">
+                    <p class="bimbingan-sosial-94a">Bimbingan Sosial</p>
+                    <img class="item-4293766-1-fYi" src="./assets/-tEa.png" />
+                </div>
+                <div class="group-8-wFL">
+                    <p class="bimbingan-karir-G2i">Bimbingan Karir</p>
+                    <img class="item-4557388-1-BQa" src="./assets/-DSW.png" />
+                </div>
+                <div class="group-9-4jG">
+                    <p class="bimbingan-belajar-PWe">Bimbingan Belajar</p>
+                    <img class="item-2482907-1-JtW" src="./assets/-6qc.png" />
+                </div>
+            </div>
+            <p class="meet-our-great-guidance-counseling-teachers-zFY">Meet our Great Guidance Counseling Teachers</p>
+            <div class="auto-group-cmr6-g8N">
+                <div class="group-11-bFL">Ricky </div>
+                <div class="group-14-pNz">Sheila </div>
+            </div>
+            <div class="auto-group-jtee-raa">
+                <div class="group-15-nDL">Kasandra Fitriani N</div>
+                <div class="group-13-dze">Gunawan</div>
+            </div>
+            <p class="about-us-7Q2">About Us</p>
+            <div class="group-16-2mt">
+                <div class="auto-group-wrev-x9k">
+                    <img class="vector-VfU" src="./assets/vector-6ii.png" />
+                    <img class="vector-3BC" src="./assets/vector-LsG.png" />
+                </div>
+                <div class="auto-group-vzm8-agv">
+                    <p class="layanan-konseling-berkualitaskami-menyediakan-layanan-konseling-yang-ramah-dan-terpercaya-dalam-sesi-konseling-online-kami-menawarkan-ruang-aman-di-mana-siswa-dapat-berbicara-tentang-masalah-mereka-membagikan-pengalaman-dan-menerima-dukungan-emosional-konselor-kami-akan-mendengarkan-dengan-penuh-perhatian-memberikan-dorongan-serta-membantu-siswa-mengembangkan-keterampilan-dan-strategi-untuk-menghadapi-tantangan-yang-mereka-hadapi-KeW">
+                        Layanan Konseling Berkualitas
+                        <br />
+                        Kami menyediakan layanan konseling yang ramah dan terpercaya. Dalam sesi konseling online, kami menawarkan ruang aman di mana siswa dapat berbicara tentang masalah mereka, membagikan pengalaman, dan menerima dukungan emosional. Konselor kami akan mendengarkan dengan penuh perhatian, memberikan dorongan, serta membantu siswa mengembangkan keterampilan dan strategi untuk menghadapi tantangan yang mereka hadapi.
+                    </p>
+                    <p class="keamanan-dan-kerahasiaankami-sangat-memprioritaskan-privasi-dan-kerahasiaan-informasi-setiap-sesi-konseling-dilakukan-dengan-kebijakan-privasi-yang-ketat-dan-data-pribadi-anda-akan-dijaga-kerahasiaannya-kami-mengadopsi-tindakan-keamanan-teknis-yang-kuat-untuk-memastikan-bahwa-semua-informasi-yang-anda-berikan-dilindungi-3U2">
+                        Keamanan dan Kerahasiaan
+                        <br />
+                        Kami sangat memprioritaskan privasi dan kerahasiaan informasi. Setiap sesi konseling dilakukan dengan kebijakan privasi yang ketat dan data pribadi Anda akan dijaga kerahasiaannya. Kami mengadopsi tindakan keamanan teknis yang kuat untuk memastikan bahwa semua informasi yang Anda berikan dilindungi
+                    </p>
+                </div>
+                <div class="auto-group-g7gr-Sm4">
+                    <img class="vector-PAW" src="./assets/vector-ERt.png" />
+                    <img class="vector-vw8" src="./assets/vector-VEv.png" />
+                </div>
+                <div class="auto-group-wrke-t7G">
+                    <p class="tim-profesionalkami-memiliki-tim-terdiri-dari-konselor-berlisensi-dan-berpengalaman-dalam-bidang-bimbingan-dan-konseling-konselor-kami-memiliki-latar-belakang-pendidikan-yang-kuat-dan-pengetahuan-mendalam-tentang-isu-isu-yang-dihadapi-oleh-siswa-seperti-stres-akademik-masalah-sosial-kecemasan-depresi-masalah-keluarga-dan-banyak-lagi-mereka-terlatih-untuk-memberikan-bimbingan-dukungan-dan-strategi-yang-dapat-membantu-siswa-mencapai-potensi-penuh-mereka-Rcz">
+                        Tim Profesional
+                        <br />
+                        Kami memiliki tim terdiri dari konselor berlisensi dan berpengalaman dalam bidang bimbingan dan konseling. Konselor kami memiliki latar belakang pendidikan yang kuat dan pengetahuan mendalam tentang isu-isu yang dihadapi oleh siswa, seperti stres akademik, masalah sosial, kecemasan, depresi, masalah keluarga, dan banyak lagi. Mereka terlatih untuk memberikan bimbingan, dukungan, dan strategi yang dapat membantu siswa mencapai potensi penuh mereka.
+                    </p>
+                    <p class="kami-berharap-dapat-membantu-anda-dalam-perjalanan-anda-menuju-kesejahteraan-emosional-dan-kesuksesan-pribadi-jangan-ragu-untuk-menghubungi-kami-jika-anda-memiliki-pertanyaan-lebih-lanjut-atau-ingin-memulai-sesi-konseling-terima-kasih-telah-memilih-layanan-konseling-bk-online-Krn">Kami berharap dapat membantu Anda dalam perjalanan Anda menuju kesejahteraan emosional dan kesuksesan pribadi. Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan lebih lanjut atau ingin memulai sesi konseling. Terima kasih telah memilih Layanan Konseling BK Online!</p>
+                </div>
             </div>
         </div>
-    </section>
-
-    <section id="contact" style="margin-bottom: 5vw; margin-top: 5vw;">
-        <div class="container">
-            <h2>Hubungi Kami</h2>
-            <p>Jika Anda membutuhkan bantuan, memiliki masalah dengan layanan kami, atau memiliki pertanyaan lebih
-                lanjut tentang SAC, silakan hubungi kami melalui
-                formulir di bawah ini.</p>
-            <form>
-                <input type="text" placeholder="Nama" required style="height: 25px;">
-                <input type="email" placeholder="Email" required style="height: 25px;"> <br>
-                <textarea placeholder="Pesan" required style="height: 100px; width: 317px; margin-top: 10px;"></textarea> <br>
-                <button type="submit" class="btn-kontak">Kirim Pesan</button>
-            </form>
+        <div class="auto-group-wmq2-Ut6">
+            <img class="vector-cDc" src="./assets/vector.png" />
+            <p class="asc-wmg">2023 ASC </p>
         </div>
-    </section>
-
-    <footer>
-        <div class="footer-container">
-            <p>&copy; 2023 SWITCHBLADE-FUTUR3 Productions. All rights reserved.</p>
-        </div>
-    </footer>
-
-    <script src="{{ asset('/js/script.js') }}"></script>
+    </div>
 </body>
-
-</html>
